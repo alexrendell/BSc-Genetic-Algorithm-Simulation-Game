74e62b9ca696e781730d6419a1ab87d99f912bb8
Alex <alexgrendell@hotmail.com>
2024-03-12 19:26:02 +0000

    Before changing order of actions

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     
     #Generation parameters:
     #number or turns, population size, maximum number of generations, starting resources, starting workers
-    test_generation = Algorithm(10, 13, 500000, 300, 10)
+    test_generation = Algorithm(10, 13, 500, 300, 10)
     test_generation.run_genetic_algorithm()
     
     print(test_generation)
